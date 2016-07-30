c7a2f33dff56aca05a1369f775913a3e58ad0f0d
surajjoshi <mailspj@gmail.com>
2016-07-30 16:30:33 -0700

    Initial commit

---
+++ requester.py
@@ -0,0 +1,72 @@
+import csv
+import pandas as pd
+import urllib2
+import os
+
+def url_to_csv(url, fname):
+    '''
+    Takes a URL to a CSV file, downloads it, and saves it to a file.
+    '''
+    # url = 'http://winterolympicsmedals.com/medals.csv'
+
+    response = urllib2.urlopen(url)
+    csv_f = csv.reader(response)
+    medals = []
+
+    for row in csv_f:
+        medals.append(row[0:20])
+
+    with open(fname, 'w') as csv_f:
+        csv_w = csv.writer(csv_f)
+        csv_w.writerows(medals)
+        csv_f.close()
+
+
+def batch_url_to_csv(urls, fnames):
+    '''
+    Takes a list of URLs to CSV files, downloads them, and saves them
+    to files given by the list of names in fnames.
+    Returns a list of the filenames saved.
+    '''
+    filename_list = []
+
+    for i in range(len(urls)):
+        try:
+            site = urllib2.urlopen(urls[i])
+            csv_f = csv.reader(site)
+            medals = []
+            for row in csv_f:
+                medals.append(row[0:20])
+
+            with open(fnames[i], 'w') as csv_f:
+                csv_w = csv.writer(csv_f)
+                csv_w.writerows(medals)
+                csv_f.close()
+                file_path = os.path.join(os.getcwd(), fnames[i])
+                filename_list.append(file_path)
+        except Exception:
+            print 'Error processing site'
+
+    # print 'file name is : ',filename_list
+    return filename_list
+
+def url_to_df(url):
+    '''
+    Takes a URL to a CSV file and returns the contents of the URL as a Pandas DataFrame.
+    '''
+    pandas_df = pd.read_csv(url)
+    return pandas_df
+
+if __name__ == "__main__":
+    # url_to_csv('http://winterolympicsmedals.com/medals.csv', 'test.csv')
+    #
+    list_of_urls = ['', 'http://winterolympicsmedals.com/medals.csv', 'http://winterolympicsmedals.com/medals.csv']
+    list_of_fname = ['file1.csv', 'file2.csv', 'file3.csv']
+    # list_of_urls = ['http://winterolympicsmedals.com/medals.csv', 'run', 'http://surajprasadjoshi.com',
+    #                     'http://winterolympicsmedals.com/medals.csv', 'jump', 'http://surajprasadjoshi.com', ]
+    # list_of_fname = ['test41.csv', 'test42.csv', 'test43.csv', 'test44.csv', 'test45.csv', 'test46.csv']
+    # batch_url_to_csv(list_of_urls, list_of_fname)
+
+
+
+    # (url_to_df('http://winterolympicsmedals.com/medals.csv'))
---
+++ test.py
@@ -0,0 +1,82 @@
+import unittest
+from requester import url_to_csv, batch_url_to_csv, url_to_df
+import urllib2
+import os
+import pandas as pd
+
+class TestFunThings(unittest.TestCase):
+    def setUp(self):
+        self.site1 = 'http://winterolympicsmedals.com/medals.csv'
+        self.site2 = 'http://surajprasadjoshi.com'
+        self.site3 = ''
+        self.site4 = 'http://winterolympicsmedals.com/medals.csv'
+        self.list_unparsable = ['http://surajprasfadjoshi.com','http://josfhisurajprasad.com']
+        self.list_invalid = ['run','jump','roll']
+        self.list_type = ['http://github.com','http://github.com', 'http://github.com']
+        self.list_mixbag = ['http://winterolympicsmedals.com/medals.csv','run', 'http://surajpradddsadjoshi.com','https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data','jump', 'http://surajprasadjoshi.com']
+        self.filelist_mixbag = ['test41.csv','test42.csv','test43.csv','test44.csv','test45.csv','test46.csv']
+        self.filelist1 = ['test1.csv','test2.csv','test3.csv']
+
+    def test_url_unparsable(self):
+        self.assertRaises(TypeError, url_to_csv, self.site1)
+
+    def test_url_error(self):
+        self.assertRaises(urllib2.URLError, url_to_csv, self.site2, 'test.csv')
+
+    def test_url_invalid(self):
+        self.assertRaises(ValueError, url_to_csv, self.site3, 'test_site4.csv')
+
+    def test_batch_no_url_error(self):
+        for file in os.listdir(os.getcwd()):
+            if file.endswith('.csv'):
+                os.remove(file)
+        # self.assertRaises(urllib2.URLError, batch_url_to_csv, self.list_unparsable, self.filelist1)
+        # self.assertRaises(ValueError, batch_url_to_csv, self.list_invalid, self.filelist1 )
+        self.assertRaises(TypeError, batch_url_to_csv, self.list_type)
+        raise RuntimeError
+
+    def test_batch_valid_url_number(self):
+        for file in os.listdir(os.getcwd()):
+            if file.endswith('.csv'):
+                os.remove(file)
+        self.assertEquals(batch_url_to_csv(self.list_mixbag, self.filelist_mixbag),\
+            [os.path.abspath(thefile) for thefile in os.listdir(os.getcwd()) if thefile.endswith(".csv")])
+
+
+    def test_batch_url_different(self):
+        for file in os.listdir(os.getcwd()):
+            if file.endswith('.csv'):
+                os.remove(file)
+        files5 = batch_url_to_csv(self.list_mixbag, self.filelist_mixbag)
+        segment1 = open(files5[0])
+        segment2 = open(files5[1])
+        self.assertNotEqual(segment1.readline(), segment2.readline())
+
+    def test_batch_correct_filenames(self):
+        # for i in range(len(batch_url_to_csv(self.list_mixbag, self.filelist_mixbag))):
+        self.assertEqual(batch_url_to_csv(self.list_mixbag, self.filelist_mixbag)[0],os.path.abspath(self.filelist_mixbag[0]))
+        self.assertEqual(batch_url_to_csv(self.list_mixbag, self.filelist_mixbag)[1], os.path.abspath(
+            self.filelist_mixbag[3]))
+
+    def test_batch_correct_number_files(self):
+        self.assertEquals(len(batch_url_to_csv(self.list_mixbag, self.filelist_mixbag)), 2)
+
+    def test_url_duplicate(self):
+        duplicate_list = []
+        for i, a in enumerate(self.list_mixbag):
+            if self.list_mixbag[i] in duplicate_list:
+                raise AssertionError("Duplicate URLs cannot be present in the parameter 'urls'.")
+            else:
+                duplicate_list.append(self.list_mixbag[i])
+
+    def test_df_returnsPdDf(self):
+        self.assertTrue(type(url_to_df(self.site1)), pd.DataFrame())
+
+'''
+    def test_df_equal_rows(self)
+        var10 = url_to_csv()
+        res = add(3,4)
+        self.assertEqual(res,7)
+
+'''
+# python -m unittest discover
